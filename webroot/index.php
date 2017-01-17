e027f08626ef2dc99b102fc409887f1aa5e3373c
Anton <anton@anton.malchuzhenko>
2017-01-17 19:02:46 +0200

    готовлю views

---
+++ webroot/index.php
@@ -0,0 +1,18 @@
+<?php 
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', dirname(dirname(__FILE__)));
+define('URI', $_SERVER['REQUEST_URI']);
+
+require_once(ROOT.DS.'lib'.DS.'init.php');
+
+/*$router = new Router(URI);
+print "<pre>";
+print_r("Route: ". $router->getRoute().PHP_EOL);
+print_r("Language: ". $router->getLanguage().PHP_EOL);
+print_r("Controller: ". $router->getController().PHP_EOL);
+print_r("Action to be called: ". $router->getMethodPrefix().$router->getAction().PHP_EOL);
+print "params: ";
+print_r($router->getParams());
+print "</pre>";*/
+
+App::run(URI);
