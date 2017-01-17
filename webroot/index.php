410afa6c6969f4188defc17154b74c6b8fa1734d
Anton Malchuzhenko <anton.malchuzhenko@gmail.com>
2017-01-17 20:03:03 +0200

    Коменты после ;)

--- webroot/index.php
+++ webroot/index.php
@@ -2,6 +2,7 @@
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(dirname(__FILE__)));
 define('URI', $_SERVER['REQUEST_URI']);
+define('VIEWS_PATH', ROOT.DS.'views');
 
 require_once(ROOT.DS.'lib'.DS.'init.php');
 
